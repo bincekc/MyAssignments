d5810aaea66714301eb3c47a7f216befa928b5c6
Bince k c <62138187+bincekc@users.noreply.github.com>
2023-12-27 14:37:13 +0530

    Add files via upload

---
+++ OddEven.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstConsoleApp
+{
+    internal class OddEven
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter a number");
+           int n= int.Parse(Console.ReadLine());
+            if (n % 2 == 0)
+            {
+                Console.WriteLine("The number is even");
+            }
+            else
+            {
+                Console.WriteLine("The Num is odd");
+            }
+        }
+    }
+}
