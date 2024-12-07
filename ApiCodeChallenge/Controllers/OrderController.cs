bd360629b293bdee32811d45ac8e9e35015ade90
Bince k c <62138187+bincekc@users.noreply.github.com>
2024-12-07 10:55:21 +0530

    Update OrderController.cs

--- ApiCodeChallenge/Controllers/OrderController.cs
+++ ApiCodeChallenge/Controllers/OrderController.cs
@@ -7,5 +7,40 @@ namespace ApiCodeChallenge.Controllers
     [ApiController]
     public class OrderController : ControllerBase
     {
+        private readonly IOrderRepository _orderRepo;
+
+    public OrderController(IOrderRepository orderRepo)
+    {
+        _orderRepo = orderRepo;
+    }
+    [HttpPost]
+    [Route("addOrder")]
+    public IActionResult PlaceOrder(Order order)
+    {
+        if (order != null)
+        {
+            _orderRepo.AddOrder(order);
+            return StatusCode(200, "Order Placed Succesfully");
+        }
+        else
+        {
+            return BadRequest();
+        }
+    }
+    [HttpGet]
+    [Route("ViewOrder")]
+    public IActionResult GetOrder()
+    {
+        var order = _orderRepo.GetAll();
+        return StatusCode(200, order);
+    }
+    [HttpDelete]
+    [Route("DeleteProductfromOrder")]
+    public IActionResult DeleteOrder(int orderId, int productId)
+    {
+        _orderRepo.DeleteProductFromOrder(orderId, productId);
+        return StatusCode(200, "Deleted Succesfully");
+    }
+}
     }
 }
